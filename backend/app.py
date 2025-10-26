bc9677b4b9fde87e215154721a156a23e860c494
TakiTyler <takimototyler@gmail.com>
2025-10-25 21:33:20 -0400

    adding genuine logic for chats

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,8 @@ CORS(app)
 
 # calling gemini & getting api key
 genai.configure()
+model = genai.GenerativeModel("gemini-2.5-flash")
+chat = model.start_chat()
 
 ### api routes ###
 
@@ -51,20 +53,21 @@ def register():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-  
-
 # testing gemini api
 @app.route("/test", methods=["GET"])
-def test():
+def test(conceptMain=str, subCategories=list, knowledgeLevel=str, context=str):
+
+    # role defines who said what "MODEL" or "USER"
+    # parts defines what was sent "TEXT" or "IMAGE"
+    jsonTemplate = {
+        "role": "",
+        "parts": ["", ""]
+    }
 
     # understanding: 1 -> 4
     # context: general, exam, work
 
-    conceptMain = "HTML"
-    subCategories = ["Elements", "Attributes", "Comments", "Clean Code"]
-    knowledgeLevel = "1"
-    context = "School"
-
+    # creating a singular string that contains all elements in subCategories
     subCategories = ", ".join(subCategories)
 
     inputtedFields = []
@@ -73,12 +76,6 @@ def test():
     inputtedFields.append(subCategories)
     inputtedFields.append(knowledgeLevel)
     inputtedFields.append(context)
-
-    print(inputtedFields)
-
-    model = genai.GenerativeModel("gemini-2.5-flash")
-
-    chat = model.start_chat()
 
     prompt = ""
 
@@ -91,6 +88,7 @@ def test():
                        "{inputtedKnowledgeLevel}", 
                        "{inputtedContext}"]
 
+    # opening prompt file & appending data
     try:
         with open(file=localPath, mode='r') as promptFile:
             prompt = promptFile.read()
@@ -100,17 +98,101 @@ def test():
         print("Prompt file was not found")
         return
 
+    chat = model.start_chat() # starting a chat so gemini remembers everything
+
+    # sending this to JSON too
+    newUserJson = jsonTemplate
+    newUserJson["role"] = "user"
+    newUserJson["parts"][0] += prompt
+
+
+    # the main logic right now for text
     while prompt != "null":
 
         response = chat.send_message(prompt, stream=True)
 
         for chunk in response:
             print(chunk.text, end="", flush=True)
+            newModelJson = jsonTemplate
+            newModelJson["role"] = "model"
+            newModelJson["parts"][0] += chunk.text
+
+        # SEND JSON FOR MODEL
         
         print("\nChat: ", end="")
         prompt = input()
 
+        newUserJson = jsonTemplate
+        newUserJson["role"] = "user"
+        newUserJson["parts"][0] += prompt
+
+        # SEND JSON FOR USER
+
     return
+
+# BUILDS A NEW MODEL
+@app.route("/newChat", methods="GET")
+def newChat():
+
+    data = request.json()
+    conceptMain = data.get("conceptMain")
+    subCategories = data.get("subCategories")
+    knowledgeLevel = data.get("knowledgeLevel")
+    context = data.get("context")
+
+    # creating a singular string that contains all elements in subCategories
+    subCategories = ", ".join(subCategories)
+
+    inputtedFields = []
+
+    inputtedFields.append(conceptMain)
+    inputtedFields.append(subCategories)
+    inputtedFields.append(knowledgeLevel)
+    inputtedFields.append(context)
+
+    # starting a chat so gemini remembers everything
+    chat = model.start_chat()
+
+    prompt = ""
+
+    localPath = f"{Path.cwd()}\\backend\\prompt.txt"
+    serverPath = ""
+
+    # we do this to create unique prompts for each instance
+    replaceKeywords = ["{inputtedConceptMain}", 
+                    "{inputtedConcepts}", 
+                    "{inputtedKnowledgeLevel}", 
+                    "{inputtedContext}"]
+
+    # opening prompt file & appending data
+    try:
+        with open(file=localPath, mode='r') as promptFile:
+            prompt = promptFile.read()
+            for i, keyword in enumerate(replaceKeywords):
+                prompt = prompt.replace(keyword, inputtedFields[i])
+    except FileNotFoundError:
+        print("Prompt file was not found")
+        return
+        
+@app.route("/returningChat", methods="GET")
+def returningChat():
+
+    data = request.json()
+    history = data.get("history")
+
+    chat = model.start_chat(history=history)
+
+    return
+
+def getChatHistory(sessionID):
+    
+    chatHistory = []
+
+    # query the chat database for everything from sessionID
+
+    
+    
+    return chatHistory
 
 # text + image submitting
 @app.route("/submit", methods=["POST"])
@@ -144,7 +226,6 @@ def submit():
         ]        
         '''
 
-        model = genai.GenerativeModel("gemini-2.5-flash")
         response = model.generate_content(
             [prompt, {"mime_type": "image/png", "data": image_bytes}]
         )
