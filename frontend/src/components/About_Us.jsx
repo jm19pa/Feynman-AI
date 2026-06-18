b06bd5dd809286f9c3826a1f1090c84553b00f2b
Tyler Takimoto <148297273+TakiTyler@users.noreply.github.com>
2026-06-18 17:29:47 -0400

    getting basic page setup working

---
+++ frontend/src/components/About_Us.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+// import './About_Us.css'
+
+function About_Us() {
+    return (
+        <div>
+            <h1>About Us</h1>
+        </div>
+    );
+}
+
+export default About_Us;
---
+++ frontend/src/components/Home_Page.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+// import './Home_Page.css'
+
+function Home_Page() {
+    return (
+        <>
+            <h1>New fAInman website!</h1>
+            <div>
+                <a href='about_us'>About Us</a>
+            </div>
+            <div className='temp1'>
+                <h2><u>The web pages we will have:</u></h2>
+                <div className='details'>
+                    <h3>Home</h3>
+                    <ul>
+                        <li>Brief description of the application</li>
+                        <li>Demo video</li>
+                    </ul>
+                </div>
+                <div className='details'>
+                    <h3>Login / Signup</h3>
+                    <ul>
+                        <li>Login: Username / Password</li>
+                        <li>Signup: Username / Password / Confirm Password / Email</li>
+                        <li>Forgot password</li>
+                    </ul>
+                </div>
+                <div className='details'>
+                    <h3>Email Verification Page</h3>
+                    <ul>
+                        <li>6 digit confirmation</li>
+                        <li>Re-send code</li>
+                    </ul>
+                </div>
+                <div className='details'>
+                    <h3>About Us</h3>
+                    <ul>
+                        <li>Description of all of those who worked on the project</li>
+                    </ul>
+                </div>
+                <div className='details'>
+                    <h3>Main</h3>
+                    <h4>------ Chat ------</h4>
+                    <ul>
+                        <li>Fully fleshed out question & response</li>
+                        <li>Saves chat history per user</li>
+                        <li>Multiple chats per person</li>
+                    </ul>
+                    <h4>------ Drawing Canvas ------</h4>
+                    <ul>
+                        <li>Can send image & text, just image, or just text per response</li>
+                        <li>Images get saved as possibly compressed?</li>
+                        <li>Draw, color, fill, erase, brush sizes</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home_Page;
