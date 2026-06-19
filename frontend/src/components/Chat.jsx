a6d0eb907b91f6154222efd73685dcb72c9354ad
Tyler Takimoto <148297273+TakiTyler@users.noreply.github.com>
2026-06-19 11:11:47 -0400

    found a different canvas thing

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -7,10 +7,10 @@ function Chat() {
             <h1>Chat</h1>
             <form>
                 <label for='send_message'>Send a message</label>
-                <input type='text' id='send_message' name='send_message'/>
-                <input type='submit' value='submit'/>
+                <input type='text' id='send_message' name='send_message' />
+                <input type='submit' value='submit' />
             </form>
-            {/* https://github.com/dilidili/react-drawing-board */}
+            {/* https://www.tldraw.com/ */}
         </>
     );
 }
