60c71c91a9c3f6c733bca210fa455ce2ff88f3e1
Tyler Takimoto <148297273+TakiTyler@users.noreply.github.com>
2026-08-23 22:09:37 -0400

    Added: Todo item jsx

--- frontend/src/components/todo/Todo.jsx
+++ frontend/src/components/todo/Todo.jsx
@@ -1,102 +1,52 @@
 import { useState } from 'react'
+import TodoItem from './todo-item/TodoItem';
 
 function Todo() {
     return (
         <section className='p-4 lg:p-0 lg:pb-6 lg:pt-6 min-h-svh bg-neutral-50/1'>
             <div className='flex items-center flex-col lg:w-75vw max-w-7xl m-auto'>
                 <h1 className='text-5xl font-extrabold'>TODO List</h1>
+
+                <p>Leave styling for Val</p>
+                <p>Let Juan do the Database stuff</p>
+
                 <div className='flex flex-wrap justify-center gap-5 pt-6'>
 
-                    <div className='border bg-purple-500/25 border-purple-400/25 rounded-4xl flex flex-col items-center w-md hover:scale-110 transition'>
-                        <h3 className='text-3xl font-semibold'>Nav Bar</h3>
+                    <TodoItem
+                        title={'Home Page'}
+                        goals={['Description of application', 'Project exigence', 'Demo video']}
+                        todo={['Styling', 'Write exigence', 'Create description', 'Record video']}
+                    />
 
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>Goals</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Simple & Stylish</li>
-                            <li>All pages accessible</li>
-                        </ul>
+                    <TodoItem
+                        title={'Login'}
+                        goals={['Username & Password', 'Forgot password']}
+                        todo={['Styling', 'Database to store users']}
+                    />
 
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>TODO</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Styling</li>
-                        </ul>
-                    </div>
+                    <TodoItem
+                        title={'Signup'}
+                        goals={['Username, Password, Confirm Password, Email', 'Forgot password']}
+                        todo={[]}
+                    />
 
-                    <div className='border bg-purple-500/25 border-purple-400/25 rounded-4xl flex flex-col items-center w-md hover:scale-110 transition'>
-                        <h3 className='text-3xl font-semibold'>Todo</h3>
+                    <TodoItem
+                        title={'Email Verification Page'}
+                        goals={['6 digit confirmation', 'Re-send code']}
+                        todo={[]}
+                    />
 
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>Goals</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Description of application</li>
-                            <li>Project exigence</li>
-                            <li>Demo video</li>
-                        </ul>
+                    <TodoItem
+                        title={'About Us'}
+                        goals={['Description of all of those who worked on the project']}
+                        todo={[]}
+                    />
 
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>TODO</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Styling</li>
-                            <li>Write exigence</li>
-                            <li>Create description</li>
-                            <li>Record video</li>
-                        </ul>
-                    </div>
-
-                    <div className='border bg-purple-500/25 border-purple-400/25 rounded-4xl flex flex-col items-center w-md hover:scale-110 transition'>
-                        <h3 className='text-3xl font-semibold'>Login</h3>
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>Goals</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Username & Password</li>
-                            <li>Forgot password</li>
-                        </ul>
-
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>TODO</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Styling</li>
-                            <li>Database to store users</li>
-                        </ul>
-                    </div>
-
-                    <div className='border bg-purple-500/25 border-purple-400/25 rounded-4xl flex flex-col items-center w-md hover:scale-110 transition'>
-                        <h3 className='text-3xl font-semibold'>Signup</h3>
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>Goals</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Username, Password, Confirm Password, Email</li>
-                            <li>Forgot password</li>
-                        </ul>
-                    </div>
-
-                    <div className='border bg-purple-500/25 border-purple-400/25 rounded-4xl flex flex-col items-center w-md hover:scale-110 transition'>
-                        <h3 className='text-3xl font-semibold'>Email Verification Page</h3>
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>Goals</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>6 digit confirmation</li>
-                            <li>Re-send code</li>
-                        </ul>
-                    </div>
-
-                    <div className='border bg-purple-500/25 border-purple-400/25 rounded-4xl flex flex-col items-center w-md hover:scale-110 transition'>
-                        <h3 className='text-3xl font-semibold'>About Us</h3>
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>Goals</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Description of all of those who worked on the project</li>
-                        </ul>
-                    </div>
-
-                    <div className='border bg-purple-500/25 border-purple-400/25 rounded-4xl flex flex-col items-center w-md hover:scale-110 transition'>
-                        <h3 className='text-3xl font-semibold'>Main</h3>
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>Chat</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Fully fleshed out question & response</li>
-                            <li>Saves chat history per user</li>
-                            <li>Multiple chats per person</li>
-                        </ul>
-                        <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>Drawing Canvas</h4>
-                        <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
-                            <li>Can send image & text, just image, or just text per response</li>
-                            <li>Images get saved as possibly compressed?</li>
-                            <li>Draw, color, fill, erase, brush sizes</li>
-                        </ul>
-                    </div>
+                    <TodoItem
+                        title={'Chat'}
+                        goals={['Question & Response', 'Save chat history', 'Multiple conversations', 'Send images', 'Compress images', 'Draw, color, fill, erase, brush sizes']}
+                        todo={[]}
+                    />
                 </div>
             </div>
         </section>
--- frontend/src/components/todo/todo-item/TodoItem.jsx
+++ frontend/src/components/todo/todo-item/TodoItem.jsx
@@ -1,6 +1,28 @@
-function TodoItem(){
+function TodoItem({ title, goals, todo }) {
 
-    return('hi')
+    return (
+        <div className='border bg-purple-500/25 border-purple-400/25 rounded-4xl flex flex-col items-center w-md hover:scale-110 transition'>
+            <h3 className='text-3xl font-semibold'>{title}</h3>
+
+            <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>Goals</h4>
+            <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
+                {
+                    goals.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))
+                }
+            </ul>
+
+            <h4 className='text-xl font-light opacity-65 underline text-left w-full pl-6'>TODO</h4>
+            <ul className='text-xl list-decimal text-left w-full pl-8 pb-2'>
+                {
+                    todo.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
 
 }
 
