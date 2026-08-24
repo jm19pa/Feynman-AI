551b0a9a1a543ad43bb6ca2096ba32fe12f2ecca
Tyler Takimoto <148297273+TakiTyler@users.noreply.github.com>
2026-08-23 22:12:42 -0400

    Styling: AboutUs Page

--- frontend/src/components/about/AboutUs.jsx
+++ frontend/src/components/about/AboutUs.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 
 function AboutUs() {
     return (
-        <>
-            <h1>About Us</h1>
-            {/* Below is the format for each of us in the about-me section */}
-            <div>
-                <h2>Name</h2>
-                <p>Picture</p>
-                <p>Major / Role / Graduating Class</p>
-                <p>Links to socials / portfolio</p>
-                <p>Brief description of self</p>
+        <section id='AboutUs' className='p-4 lg:p-0 lg:pb-6 min-h-svh bg-neutral-50/1'>
+            {/* This div holds all the content, limiting the width */}
+            <div className='flex items-center flex-col lg:w-75vw max-w-7xl m-auto'>
+                <h1 className='text-5xl font-extrabold'>About Us</h1>
+                <div className='text-md md:text-lg lg:text-xl'>
+                    <p className='opacity-50'>Format for about page</p>
+                    <h2>Name</h2>
+                    <p>Picture</p>
+                    <p>Major / Role / Graduating Class</p>
+                    <p>Links to socials / portfolio</p>
+                    <p>Brief description of self</p>
+                </div>
             </div>
-        </>
+        </section>
     );
 }
 
