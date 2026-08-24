c16526a087d5aaf25559bef7256c2caa4fdfc7fc
Tyler Takimoto <148297273+TakiTyler@users.noreply.github.com>
2026-08-23 22:31:44 -0400

    Styling: Signup

--- frontend/src/components/signup/SignUp.jsx
+++ frontend/src/components/signup/SignUp.jsx
@@ -2,20 +2,33 @@ import { useState } from 'react'
 
 function SignUp() {
     return (
-        <>
-            <h1>Sign Up</h1>
-            <form>
-                <label for='email'>Email</label>
-                <input type='text' id='email' name='email'/>
-                <label for='username'>Username</label>
-                <input type='text' id='username' name='username'/>
-                <label for='password'>Password</label>
-                <input type='text' id='password' name='password'/>
-                <label for='conf_password'>Confirm Password</label>
-                <input type='text' id='conf_password' name='conf_password'/>
-                <input type='submit' value='submit'/>
-            </form>
-        </>
+        <section id='Signup' className='p-4 lg:p-0 lg:pb-6 lg:pt-6 min-h-svh bg-neutral-50/1'>
+            {/* This div holds all the content, limiting the width */}
+            <div className='flex items-center flex-col lg:w-75vw max-w-7xl m-auto'>
+                <h1 className='text-5xl font-extrabold'>Signup</h1>
+                <form className='text-md md:text-lg lg:text-xl pt-4 flex flex-col items-center'>
+                    <div className='flex flex-col items-center'>
+                        <label for='email'>Email</label>
+                        <input type='text' id='email' name='email' className='border bg-fuchsia-500/25 border-fuchsia-400/25 text-center rounded-full' />
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <label for='username'>Username</label>
+                        <input type='text' id='username' name='username' className='border bg-fuchsia-500/25 border-fuchsia-400/25 text-center rounded-full' />
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <label for='password'>Password</label>
+                        <input type='text' id='password' name='password' className='border bg-fuchsia-500/25 border-fuchsia-400/25 text-center rounded-full' />
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <label for='conf_password'>Confirm Password</label>
+                        <input type='text' id='conf_password' name='conf_password' className='border bg-fuchsia-500/25 border-fuchsia-400/25 text-center rounded-full' />
+                    </div>
+                    <div className='pt-4'>
+                        <input type='submit' value='submit' className='border bg-rose-500/25 border-rose-400/25 rounded-4xl w-fit pl-4 pr-4' />
+                    </div>
+                </form>
+            </div>
+        </section>
     );
 }
 
